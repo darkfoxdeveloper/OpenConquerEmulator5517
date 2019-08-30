068f78e4986aee89d0fa38c54cb57c28b569fd33
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2019-08-30 20:40:12 +0200

    Database optimization

--- zfserver v2.5517/DB/Mappings/Login Resource.cs
+++ zfserver v2.5517/DB/Mappings/Login Resource.cs
@@ -12,7 +12,6 @@ namespace DB.Mappings
             Id(x => x.Identity).GeneratedBy.Identity().Not.Nullable().Column("id");
             Map(x => x.UserIdentity).Not.Nullable().Default("0").Column("account_id");
             Map(x => x.LoginTime).Not.Nullable().Default("0").Column("login_time");
-            Map(x => x.OnlineSecond).Not.Nullable().Default("0").Column("online_second");
             Map(x => x.MacAddress).Not.Nullable().Default("000000000000").Column("mac_adr");
             Map(x => x.IpAddress).Not.Nullable().Default("127.0.0.1").Column("ip_adr");
             Map(x => x.ResourceSource).Not.Nullable().Default("2").Column("res_src");
